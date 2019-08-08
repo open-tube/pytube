f9fc893501348a24f5970b51814e333e28d69215
minhyeoklee <rhodochrosited@gmail.com>
2019-08-08 14:04:06 +0900

    :bug: thumbnail KeyError

--- pytube/__main__.py
+++ pytube/__main__.py
@@ -242,7 +242,7 @@ class YouTube(object):
         :rtype: str
 
         """
-        return self.player_config_args['thumbnail_url']
+        return self.player_config_args['player_response']['videoDetails']['thumbnail']
 
     @property
     def title(self):
